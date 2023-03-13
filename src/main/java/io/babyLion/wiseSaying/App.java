27d9adc7b081f417cf8f3c145f98888503ee3271
yeonann <lkim82@naver.com>
2023-03-14 01:44:58 +0900

    2단계

--- src/main/java/io/babyLion/wiseSaying/App.java
+++ src/main/java/io/babyLion/wiseSaying/App.java
@@ -18,6 +18,11 @@ public class App {
             String command = sc.nextLine().trim();
             if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String authorName = sc.nextLine().trim();
             }
         }
     }
